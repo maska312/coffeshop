e965a7148b8cf66db8d8308637b9feb9722caea3
maska312 <66679584+maska312@users.noreply.github.com>
2025-05-19 02:20:24 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
-const base = process.env.NODE_ENV === 'production' ? '/coffeeshop/' : '/';
+const base = process.env.NODE_ENV === 'production' ? '/' : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
